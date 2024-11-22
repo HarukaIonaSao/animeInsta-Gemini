8892dd0c56be8db6093aa8dc7181f2739c7e7b6b
HarukaIonaSao <robertaribeiro004@gmail.com>
2024-11-22 14:24:40 -0300

    update postsController.js, postsModels.js and postsRoutes.js

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,7 +1,19 @@
-import { getTodosPosts } from "../models/postsModels.js";
+import { getTodosPosts, criarPost } from "../models/postsModels.js";
 
 export async function listarPosts (req, res)
 {
     const posts =  await getTodosPosts();
     res.status(200).json(posts);
 }
+
+export async function postarNovoPost(req, res) {
+    const newPost = req.body;
+    try {
+        const postCriado = await criarPost(newPost)
+        res.status(200).json(postCriado);
+    } catch (erro) {
+        console.log(erro.message);
+        res.status(500).json({"Erro":"Falha no servidor"})        
+    }
+}
+
--- src/models/postsModels.js
+++ src/models/postsModels.js
@@ -7,3 +7,9 @@ export async function getTodosPosts(){
     const colecao = db.collection("animes");
     return colecao.find().toArray();
 }
+
+export async function criarPost(newPost) {
+    const db = conexao.db("imersao");
+    const colecao = db.collection("animes");
+    return colecao.insertOne(newPost);
+}
