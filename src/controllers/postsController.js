d86ac6d562359d03a033932bcdda485bccb281fa
HarukaIonaSao <robertaribeiro004@gmail.com>
2024-11-22 19:04:10 -0300

    update postsController.js

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,51 +1,42 @@
 import { getTodosPosts, criarPost } from "../models/postsModels.js";
-import fs from 'fs';
+import fs from "fs";
 
 export async function uploadImagem(req, res) {
-    console.log(req.file); // Verifique se contém path, originalname, etc.
+  console.log(req.file); // Verifique se contém path, originalname, etc.
 
-    const newPost = {
-        
-        titulo:"",
-        genero:"",
-        ano:"",
-        episodios:"",
-        imgUrl: req.file.originalname,
-        alt:""
-    };
-    try {
-        const postCriado = await criarPost(newPost);
-        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
-        fs.renameSync(req.file.path, imagemAtualizada)
-        res.status(200).json(postCriado);
-    } catch (erro) {
-        console.error(erro.message);
-        res.status(500).json({"Erro":"Falha no servidor"})        
-    }
+  const newPost = {
+    titulo: "",
+    genero: "",
+    ano: "",
+    episodios: "",
+    imgUrl: req.file.originalname,
+    alt: "",
+  };
+  try {
+    const postCriado = await criarPost(newPost);
+    const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+    fs.renameSync(req.file.path, imagemAtualizada);
+    res.status(200).json(postCriado);
+  } catch (erro) {
+    console.error(erro.message);
+    res.status(500).json({ Erro: "Falha no server" });
+  }
 }
 
-export async function listarPosts (req, res)
-{
-    const posts =  await getTodosPosts();
-    res.status(200).json(posts);
+export async function listarPosts(req, res) {
+  const posts = await getTodosPosts();
+  res.status(200).json(posts);
 }
 
 export async function postarNovoPost(req, res) {
-    const newPost = req.body;
-    try {
-        
-        const postCriado = await criarPost(newPost);
-        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
-        fs.renameSync(req.file.path, imagemAtualizada)
-        res.status(200).json(postCriado);
-    } catch (erro) {
-        console.error(erro.message);
-        res.status(500).json({"Erro":"Falha no servidor"});     
-    }
+  const newPost = req.body;
+  try {
+    const postCriado = await criarPost(newPost);
+    const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+    fs.renameSync(req.file.path, imagemAtualizada);
+    res.status(200).json(postCriado);
+  } catch (erro) {
+    console.error(erro.message);
+    res.status(500).json({ Erro: "Falha no servidor" });
+  }
 }
-
-
- 
-
-
-
