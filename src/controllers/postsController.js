a63476cb18c2fb0896f94d2b1c7acdd749f132cf
HarukaIonaSao <robertaribeiro004@gmail.com>
2024-11-22 15:06:52 -0300

    build(deps): update package-lock.json, package.json, postsController.js and postsRoutes.js

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,16 @@
-import { getTodosPosts, criarPost } from "../models/postsModels.js";
+import { getTodosPosts, criarPost, uploadImagem } from "../models/postsModels.js";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+
+const upload = multer({ dest: "./uploads" , storage})
 
 export async function listarPosts (req, res)
 {
@@ -16,4 +28,23 @@ export async function postarNovoPost(req, res) {
         res.status(500).json({"Erro":"Falha no servidor"})        
     }
 }
+
+export async function uploadImagem(req, res) {
+    const newPost = {
+        titulo:"",
+        genero:"",
+        ano:"",
+        episodios:"",
+        imgUrl:req.file.originalname,
+        alt:""
+    };
+    try {
+        const postCriado = await criarPost(newPost)
+        res.status(200).json(postCriado);
+    } catch (erro) {
+        console.log(erro.message);
+        res.status(500).json({"Erro":"Falha no servidor"})        
+    }
+}
+
 
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,11 +1,16 @@
 import express from "express";
+import multer from "multer";
 import { listarPosts, postarNovoPost } from "../controllers/postsController.js";
 
+const upload = multer({
+    dest:"./uploads"
+})
 const routes = (app) => {
     app.use(express.json());
     
     app.get("/posts", listarPosts);
     app.post("/posts",postarNovoPost);
+    app.post("/upload",uploads.single("imagem"), uploadImagem)
 }
 
 
