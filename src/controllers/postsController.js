3dd6a3ae146aa0f0f645560e3f05d478f1df6e7f
HarukaIonaSao <robertaribeiro004@gmail.com>
2024-11-23 11:51:52 -0300

    create 1 file, update 4 files, rename 1 file and delete 1 file

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,28 +1,5 @@
-import { getTodosPosts, criarPost } from "../models/postsModels.js";
+import { getTodosPosts, criarPost } from "../Model/postsModel.js";
 import fs from "fs";
-
-export async function uploadImagem(req, res) {
-  console.log(req.file); // Verifique se contém path, originalname, etc.
-
-  const newPost = {
-    titulo: "",
-    genero: "",
-    ano: "",
-    episodios: "",
-    imgUrl: req.file.originalname,
-    alt: "",
-  };
-  try {
-    const postCriado = await criarPost(newPost);
-    const imagemAtualizada = `uploads/${postCriado.insertedId}.${path.extname(req.file.originalname)}`;
-    fs.renameSync(req.file.path, imagemAtualizada);
-    res.status(200).json(postCriado);
-    console.log('Arquivo renomeado com sucesso!');
-  } catch (erro) {
-    console.error(erro.message);
-    res.status(500).json({ Erro: "Falha no server" });
-  }
-}
 
 export async function listarPosts(req, res) {
   const posts = await getTodosPosts();
@@ -39,5 +16,29 @@ export async function postarNovoPost(req, res) {
   } catch (erro) {
     console.error(erro.message);
     res.status(500).json({ Erro: "Falha no servidor" });
+  }
+}
+
+export async function uploadImagem(req, res) {
+  console.log(req.file); // Verifique se contém path, originalname, etc.
+
+  const newPost = {
+    titulo: "",
+    genero: "",
+    ano: "",
+    episodios: "",
+    imgUrl: req.file.originalname,
+    alt: "",
+  };
+
+  try {
+    const postCriado = await criarPost(newPost);
+    const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+    fs.renameSync(req.file.path, imagemAtualizada);
+    res.status(200).json(postCriado);
+    console.log('Arquivo renomeado com sucesso!');
+  } catch (erro) {
+    console.error(erro.message);
+    res.status(500).json({ Erro: "Falha no server" });
   }
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -2,63 +2,33 @@ import express from "express";
 import multer from "multer";
 import { listarPosts, postarNovoPost, uploadImagem} from "../controllers/postsController.js";
 
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, 'uploads/');
-//         console.log('Salvando em uploads/');
-//     },
-//     filename: function (req, file, cb) {
-//         cb(null, file.originalname);
-//     }
-// });
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+        console.log('Salvando em uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' +file.originalname);
+        console.log('Renomeando...');
+        
+    }
+});
 
-// const upload = multer({ storage: storage});
+
 
 const upload = multer({
-    storage: multer.diskStorage({
-      destination: 'uploads/',
-      filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname); 
-    }
-})
-});
+         dest: "./uploads", storage});
+                console.log('Salvando em uploads/');
+           
+     
 
 const routes = (app) => {
     app.use(express.json());
     
     app.get("/posts", listarPosts);
     app.post("/posts", postarNovoPost);
-    // app.post("/upload", upload.single("imagem1"), uploadImagem);
-    app.post('/upload', upload.single("imagem"), async (req,res) => {
-        try {
-            if (!req.file) {
-                return res.status(400).json({ erro: 'Nenhum arquivo foi enviado.' });
-            }
+    app.post("/upload", upload.single("imagem"), uploadImagem);
 
-            console.log(req.file); // Exibe informações do arquivo
-  // Chamada para a função uploadImagem
-
-  const allowedTypes = ['.png', '.jpg', '.jpeg', '.gif'];
-        if (!allowedTypes.includes(path.extname(req.file.originalname).toLowerCase())) {
-            return res.status(400).json({ erro: 'Tipo de arquivo não permitido.' });
-        }
-  await uploadImagem(req, res);
-              res.status(200).json({ mensagem: 'Arquivo recebido com sucesso!', file: req.file });
-
-
-        } catch (error) {
-            console.error(error);
-            return res.status(400).json({ erro: 'Erro ao fazer upload: ' + error.message });
-
-        } 
-        // if (!req.file) {
-        //     return res.status(400).json({ erro: 'Arquivo não enviado!' });
-        // }
-        // console.log("req.file:", req.file);
-console.log("postCriado:", postCriado);
-console.log("Imagem atualizada:", imagemAtualizada);
-
-    });
 }
 
 
