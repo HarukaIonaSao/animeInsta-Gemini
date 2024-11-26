12c6db35fe141be0fa58817bf3d2016cccb8acc0
HarukaIonaSao <robertaribeiro004@gmail.com>
2024-11-26 18:47:50 -0300

    add

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from "express";
-// import routes from "./src/routes/postsRoutes.js";
+import routes from "./src/routes/postsRoutes.js";
 import conectarAoBanco from "./src/config/dbConfig.js";
 
 await conectarAoBanco(process.env.STRING_CONEXAO)
 const app = express();
-app.use(express.static("uploads"))
+app.use(express.static("uploads"));
 routes(app);
 
 app.listen(3000, () => {
